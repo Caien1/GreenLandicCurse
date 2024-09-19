50b3f5115f8ab5cfe50aea34fbed2bf0c2680da2
Caien1 <pulwama01@gmail.com>
2024-09-19 18:12:27 +0530

    Init commands

--- deploy-development.js
+++ deploy-development.js
@@ -40,7 +40,7 @@ const rest = new REST().setToken(token);
 
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
-			Routes.applicationCommands(clientId,`883106939578503189`),
+			Routes.applicationCommands(clientId,guildId),
 
 			{ body: commands },
 		);
