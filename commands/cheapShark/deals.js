3e8e6b15e958edbc8eaccbd95e47a36de479ac49
Caien1 <pulwama01@gmail.com>
2024-09-20 00:50:18 +0530

    deals with pages

--- commands/cheapShark/deals.js
+++ commands/cheapShark/deals.js
@@ -1,10 +1,10 @@
-const {SlashCommandBuilder} = require('discord.js')
+const { SlashCommandBuilder } = require('discord.js')
 //const { execute } = require('../utility/user')
 const { MessageActionRow, MessageButton } = require('discord.js');
 const { Pagination } = require('pagination.djs');
 const { EmbedBuilder } = require('discord.js');
 
-const PAGE_SIZE=7;
+const PAGE_SIZE = 7;
 module.exports = {
 
     data: new SlashCommandBuilder()
@@ -12,94 +12,94 @@ module.exports = {
         .setDescription('Lists games on sale')
         .addStringOption(option =>
             option.setName('maxprice')
-            .setDescription('Only returns deals with a price less than or equal to this value (50 acts the same as no limit)')
-            .setRequired(true) 
+                .setDescription('Only returns deals with a price less than or equal to this value (50 acts the same as no limit)')
+                .setRequired(true)
         ),
 
-        async execute(interaction) {
-            const pagination = new Pagination(interaction);
-           const maxPrice = interaction.options.getString('maxprice')
-         //  const startindex = interaction.options.getString('startindex')
-            const url = `https://www.cheapshark.com/api/1.0/deals?upperPrice=${maxPrice}`
+    async execute(interaction) {
+        const pagination = new Pagination(interaction);
+        const maxPrice = interaction.options.getString('maxprice')
+        //  const startindex = interaction.options.getString('startindex')
+        const url = `https://www.cheapshark.com/api/1.0/deals?upperPrice=${maxPrice}`
 
-            async function getDeals(url) {
+        async function getDeals(url) {
 
-                try{
+            try {
 
                 const response = await fetch(url)
                 let data = await response.json()
 
-               // console.log(data.length);
+                // console.log(data.length);
                 return data
-                }
-                catch(err){
-                    console.log(err);
-                }             
             }
+            catch (err) {
+                console.log(err);
+            }
+        }
 
-           let data =await  getDeals(url);
+        let data = await getDeals(url);
 
-          console.log(data[0])
-           if(data.length==0){
+        console.log(data[0])
+        if (data.length == 0) {
 
             await interaction.reply(`No such offer`);
 
-           }
+        }
 
 
-           
-            
-            
-            const embeds = data.map(function(deal){
-                return {name:`**Title:** ${deal.title}\n`,value:`*Price*:$${deal.salePrice}\n\n`}
-            })
-           // console.log(embeds)
-        
-            pagination.setTitle('Games on sale');
-            
-            // for (let i = 0; i <= 5; i++) {
-            //     const newEmbed = new EmbedBuilder().setTitle(`Embed ${i + 1}`);
-            //     embeds.push(newEmbed);
-            // }
-            // pagination.addFields([
-            //     {
-            //         name: 'First',
-            //         value: 'First'
-            //     },
-            //     {
-            //         name: 'Second',
-            //         value: 'Second'
-            //     },
-            //     {
-            //         name: 'Third',
-            //         value: 'Third'
-            //     }
-            // ]);
+
+
+
+        const embeds = data.map(function (deal) {
+            return { name: `**Title:** ${deal.title}\n`, value: `*Price*:$${deal.salePrice}\n\n` }
+        })
+        // console.log(embeds)
+
+        pagination.setTitle('Games on sale');
+
+        // for (let i = 0; i <= 5; i++) {
+        //     const newEmbed = new EmbedBuilder().setTitle(`Embed ${i + 1}`);
+        //     embeds.push(newEmbed);
+        // }
+        // pagination.addFields([
+        //     {
+        //         name: 'First',
+        //         value: 'First'
+        //     },
+        //     {
+        //         name: 'Second',
+        //         value: 'Second'
+        //     },
+        //     {
+        //         name: 'Third',
+        //         value: 'Third'
+        //     }
+        // ]);
         pagination.addFields(embeds);
         pagination.paginateFields(true);
-            // or if you want to set a common change in all embeds, you can do it by adding a cb.
-            // pagination.setEmbeds(embeds, (embed, index, array) => {
-            //     return embed.setFooter({ text: `Page: ${index + 1}/${array.length}` })
-            //     ;
-            // });
-            pagination.render();
-        
-   
-
-
-
-           // Send the first page
-           
-          // await interaction.reply(`test`);
-                 
-
-                 
+        // or if you want to set a common change in all embeds, you can do it by adding a cb.
+        // pagination.setEmbeds(embeds, (embed, index, array) => {
+        //     return embed.setFooter({ text: `Page: ${index + 1}/${array.length}` })
+        //     ;
+        // });
+        pagination.render();
 
 
 
 
 
-         
-        },
+        // Send the first page
+
+        // await interaction.reply(`test`);
+
+
+
+
+
+
+
+
+
+    },
 
 }
