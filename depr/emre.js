0b5d5fe35712ddf2453f92bef365fe87617bea6e
Caien1 <pulwama01@gmail.com>
2024-09-19 18:13:21 +0530

    not used anymore

---
+++ depr/emre.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('emre')
+		.setDescription('Rant'),
+	async execute(interaction) {
+		await interaction.reply('Emre sucks at every game he plays' );
+	},
+};
